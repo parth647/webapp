993742c3a399fd12ea170fed83cae2e6748f232c
Parth <parth647@gmail.com>
2018-09-28 19:03:18 +0530

    Individual blog page working

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
                 url(r'^$', ListView.as_view(
                                     queryset=Post.objects.all().order_by("-date")[:25],
                                     template_name="blog/blog.html")),
+                url(r'^(?P<pk>\d+)$',DetailView.as_view(model=Post,template_name="blog/post.html"))                    
             ]
 
 # Create your views here.
